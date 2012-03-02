1a39ddd9fc3819c05b594649191b92bdc2738e30
Kunal Choudhary <Kunal.Choudhary@symphonysv.com>
2012-03-02 13:40:45 +0530

    Created a New java file

---
+++ HelloGit/src/com/symphony/poplicus/java/Hello.java
@@ -0,0 +1,9 @@
+package com.symphony.poplicus.java;
+
+public class Hello {
+String message ="Hello Git,Nice Exploring You!";
+public static void main(String[] args) {
+	Hello hello=new Hello();
+	System.out.println(hello.message);
+}
+}
