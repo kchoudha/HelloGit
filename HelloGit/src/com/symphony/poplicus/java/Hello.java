273653071153ec103ddccd76b40d9043b10f8f93
Kunal Choudhary <Kunal.Choudhary@symphonysv.com>
2012-03-02 13:42:43 +0530

    created first repository using git tutorial

--- HelloGit/src/com/symphony/poplicus/java/Hello.java
+++ HelloGit/src/com/symphony/poplicus/java/Hello.java
@@ -5,5 +5,6 @@ String message ="Hello Git,Nice Exploring You!";
 public static void main(String[] args) {
 	Hello hello=new Hello();
 	System.out.println(hello.message);
+	System.out.println("created first repository using the git tutorial");
 }
 }
